c2c9435302629ee09812a27f01ba63d2effc0d8f
student-studies-everyday <student.studies.everyday@gmail.com>
2024-05-09 17:45:02 +0900

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-const Fruit = ({ name,img }) => {
-	return (
-		<>
-			<h3>좋아하는 과일: {name}</h3>
-			<img src={img} alt={name} />
-		</>
-	);
-};
+import LifeCycle from './LifeCycle';
 const fruitDB = [
 	{ name: 'banana ', image: 'http://qwerew.cafe24.com/images/banana.png' },
 	{ name: 'orange', image: 'http://qwerew.cafe24.com/images/orange.png' },
@@ -13,13 +6,22 @@ const fruitDB = [
 	{ name: 'melon', image: 'http://qwerew.cafe24.com/images/melon.jpg' },
 ];
 
+const Fruit = ({ name, img }) => {
+	return (
+		<>
+			<h3>좋아하는 과일: {name}</h3>
+			<img src={img} alt={name} />
+		</>
+	);
+};
+
 function App() {
 	return (
 		<div>
 			<h1>도레미레시피앱</h1>
-			{fruitDB.map((value,index) => {
-				console.log('value',value);
-				return <Fruit key={index} name={value.name} img={value.image}/>;
+			<LifeCycle />
+			{fruitDB.map((value, index) => {
+				return <Fruit key={index} name={value.name} img={value.image} />;
 			})}
 		</div>
 	);
